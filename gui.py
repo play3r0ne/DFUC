bde3c5cff456334b7c9d10b7b451c871ac951191
play3r0ne <ongjoel28@gmail.com>
2025-10-12 18:08:09 +0800

    Added functionality to the program to generate a visual prediction mask.

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ import torch
 from test import iou_fn, focal_fn, dice_fn
 import numpy as np
 import time
-
+import matplotlib.pyplot as plt
 
 ## Load model
 model = smp.Unet(
@@ -30,7 +30,8 @@ if img_uploader is not None:
     if mask_uploader is not None:
         image = Image.open(img_uploader).convert("RGB")
         mask = Image.open(mask_uploader).convert("L")
-        st.image(img_uploader, caption="Image uploaded", use_column_width=True)
+        st.image(img_uploader, caption="Image uploaded", use_container_width=True)
+        st.image(mask_uploader, caption="Mask uploaded", use_container_width=True)
 
         img_transforms = A.Compose([
                 A.Resize(256, 256),
@@ -39,7 +40,6 @@ if img_uploader is not None:
                 ToTensorV2()
             ])
         
-
         def preprocess(image, mask):
             image = np.array(image)
             mask = np.array(mask)
@@ -47,11 +47,12 @@ if img_uploader is not None:
             # converting mask from 0-255 to binary
             mask = (mask > 127).astype(np.float32)
             augmented = img_transforms(image = image, mask = mask)
-            image = augmented['image']
-            mask = augmented['mask'].unsqueeze(0).float()
+            img_tensor = augmented['image']
+            mask_tensor = augmented['mask'].unsqueeze(0).float()
 
+            return img_tensor.unsqueeze(0), mask_tensor.unsqueeze(0)
 
-        img_tensor, mask_tensor = preprocess(img_uploader, mask_uploader)
+        img_tensor, mask_tensor = preprocess(image, mask)
 
         if st.button("Detect DFU"):
             start_time = time.time()
@@ -60,10 +61,14 @@ if img_uploader is not None:
             iou = iou_fn(preds, mask_tensor)
             end_time = time.time() - start_time
 
+            pred_mask = torch.sigmoid(preds)
+            pred_mask = (pred_mask > 0.5).float()
+            pred_mask_np = pred_mask.squeeze().cpu().numpy()
+            pred_mask_np = (pred_mask_np * 255).astype(np.uint8)
+
             st.write(f"IOU: {iou:.2f}% | Loss: {loss:.2f} | Time: {end_time:.2f}s")
+            st.image(pred_mask_np, caption="Predicted DFU Mask", use_column_width=True)
     else:
         st.error("Please upload a mask file.")
 else:
     st.error("Please upload an image file")
-
-
